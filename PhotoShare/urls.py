f9b789a2330e900f115488ab0f6b516034cfadb6
hekut3 <90758295+hekut3@users.noreply.github.com>
2023-12-12 01:13:52 +0300

    Add files via upload

---
+++ PhotoShare/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('feed.urls')),
+    path('users/<slug>/', user_views.profile_view, name='profile_view'),
+    path('edit-profile/', user_views.edit_profile, name='edit_profile'),
+    path('my-profile/', user_views.my_profile, name='my_profile'),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
